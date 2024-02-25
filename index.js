6db6575055cecc562e6164ea351ace223306dff9
John Aricaya <jaricaya@gmail.com>
2024-02-25 14:21:08 -0800

    DOM content loaded lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM has loadded');
+    let text = document.querySelector('#text')
+    text.textContent = 'This is really cool!';
+});
+
+
